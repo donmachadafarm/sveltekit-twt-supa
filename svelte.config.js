e40c31b7d424f1bbb0299171ad194002a2659ae9
donmacdon <don.macadat@gmail.com>
2022-05-15 21:34:51 +0800

    added vercel requirements

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ const config = {
             ssr: {
                 noExternal: dev ? [] : ['@supabase/supabase-js']
             },
+			define: {
+                'process.env': process.env,
+            },
 		}
 	},
 
