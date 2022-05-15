ff9569fd592cff39a4c5560f735552ceb7900307
donmacdon <don.macadat@gmail.com>
2022-05-15 22:29:54 +0800

    vercel changes 1.4

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,8 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from 'dotenv'
 
-export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
-export const SUPABASE_PUBLIC_KEY = import.meta.env.VITE_SUPABASE_KEY
+dotenv.config()
 
-const supabase = createClient(SUPABASE_URL,SUPABASE_PUBLIC_KEY)
+// export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+// export const SUPABASE_PUBLIC_KEY = import.meta.env.VITE_SUPABASE_KEY
+
+let {VITE_SUPABASE_URL,VITE_SUPABASE_KEY} = process.env
+
+const supabase = createClient(VITE_SUPABASE_URL,VITE_SUPABASE_KEY)
 
 export default supabase
